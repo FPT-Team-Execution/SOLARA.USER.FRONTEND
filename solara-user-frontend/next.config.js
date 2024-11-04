389cf6a3130a5e4880ebba15595cc44f60a53c38
DatSung <datsung.dev@gmail.com>
2024-11-05 00:15:30 +0700

    chore: add clerk setup middleware

---
+++ solara-user-frontend/next.config.js
@@ -0,0 +1,6 @@
+// next.config.js
+const config = {
+  // Cấu hình của bạn
+};
+
+module.exports = config;
--- solara-user-frontend/src/app/layout.tsx
+++ solara-user-frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,12 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <AntdRegistry>{children}</AntdRegistry>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
