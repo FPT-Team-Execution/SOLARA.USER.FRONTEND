eef18d762c0addd2b9c1df26c6e1998946880854
DatSung <datsung.dev@gmail.com>
2024-12-03 15:20:14 +0700

    move exercise type to exercise

--- solara-user-frontend/src/types/exercise.ts
+++ solara-user-frontend/src/types/exercise.ts
@@ -1,3 +1,5 @@
+import { IPageRequest } from "./general";
+
 export type AnswerDto = {
     id: string;
     optionText: string;
@@ -64,4 +66,35 @@ export type CreateUserAttemptRequest = {
     userId: string;
     exerciseId: string;
     options: string[];
+};
+
+export type ExerciseTypeDto = {
+    id: string;
+    name: string;
+    description: string;
+    externalLink?: string;
+    minOptions: number;
+    maxOptions?: number;
+    isMultipleChoice: boolean;
+};
+
+export type GetPagedTypesRequest = IPageRequest & {
+
+}
+
+export type UpdateTypeRequest = {
+    id: string;
+    name: string;
+    description: string;
+    externalLink?: string;
+    maxOptions?: number;
+    isMultipleChoice: boolean;
+};
+
+export type CreateTypeRequest = {
+    name: string;
+    description: string;
+    externalLink?: string;
+    maxOptions?: number;
+    isMultipleChoice: boolean;
 };
