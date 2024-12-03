ef7fd57cd681d427e5f966617062de027df3f088
DatSung <datsung.dev@gmail.com>
2024-12-03 14:58:55 +0700

    define subTopic type

---
+++ solara-user-frontend/src/types/subTopic.ts
@@ -0,0 +1,29 @@
+import { IPageRequest } from "./general";
+
+export type SubTopicDto = {
+    id: string;
+    name: string;
+    description: string;
+    totalXP: number;
+    totalExercise: number;
+    createdOn?: Date;
+    updatedOn?: Date;
+};
+
+export type GetPagedSubTopicRequest = IPageRequest & {
+
+}
+
+export type CreateSubTopicRequest = {
+    subTopicId: string;
+    topicId: string;
+    name: string;
+    description: string;
+};
+
+export type UpdateSubTopicRequest = {
+    subTopicId: string;
+    topicId: string;
+    name: string;
+    description: string;
+};
