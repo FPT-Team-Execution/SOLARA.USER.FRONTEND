7bd383c38a9f58814948bfd1d37408ff352a4bcd
DatSung <datsung.dev@gmail.com>
2024-12-08 16:53:43 +0700

    style: spinner

--- solara-user-frontend/src/components/ui/Spinner.tsx
+++ solara-user-frontend/src/components/ui/Spinner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Spinner = () => {
     return (
-        <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-green-600"></div>
+        <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-green-600"></div>
     )
 }
 
