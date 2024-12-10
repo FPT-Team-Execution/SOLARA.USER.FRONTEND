10c129f7745a9b5a55ebbe503e86624486f93df7
DatSung <datsung.dev@gmail.com>
2024-12-10 17:39:39 +0700

    feat: declare excercise page

--- solara-user-frontend/src/app/(private)/learning/topics/subs/excercises/page.tsx
+++ solara-user-frontend/src/app/(private)/learning/topics/subs/excercises/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client"
+
+import { useSearchParams } from "next/navigation";
+
 
 const Page = () => {
+
+  const searchParams = useSearchParams();
+  const subTopicId = searchParams.get('subTopicId');
+
   return (
     <div>
       This is excercise page
+      {subTopicId}
     </div>
   )
 }
