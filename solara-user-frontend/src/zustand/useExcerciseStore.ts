f501aad0e985e32124278de13ba8b06e68e294c6
DatSung <datsung.dev@gmail.com>
2024-12-14 08:30:46 +0700

    feat: get excercises and excercise axiox call api

--- solara-user-frontend/src/zustand/useExcerciseStore.ts
+++ solara-user-frontend/src/zustand/useExcerciseStore.ts
@@ -1,6 +1,8 @@
+import { GET_EXERCISES_API } from '@/constants/apis';
 import { UpdateExcerciseOptionRequest } from './../types/excercise';
 import { AddOptionRequest, CreateExcerciseRequest, CreateUserAttemptRequest, ExcerciseDto, GetPagedExcerciseTypesRequest, UpdateExcerciseRequest } from '@/types/excercise'
-import { IPaginate } from '@/types/general'
+import { IBaseModel, IPaginate } from '@/types/general'
+import axiosClient from '@/utils/axios/axiosClient';
 import { create } from 'zustand'
 
 interface ExcerciseStore {
@@ -21,8 +23,44 @@ const useExcerciseStore = create<ExcerciseStore>((set) => (
     {
         excercise: null,
         excercises: null,
-        getExcercise: async (id: string) => { },
-        getExcercises: async (subTopicId: string, query: GetPagedExcerciseTypesRequest) => { },
+
+        getExcercise: async (id: string) => {
+            try {
+                const response = await axiosClient.get<IBaseModel<ExcerciseDto>>(GET_EXERCISES_API(id))
+
+                if (!response.data.isSuccess) {
+                    return
+                }
+
+                set((state) => ({
+                    ...state,
+                    excercise: response.data.responseRequest
+                }))
+
+            } catch (error) {
+                console.log('Error fetching excercise', error)
+            }
+
+        },
+
+        getExcercises: async (subTopicId: string, query: GetPagedExcerciseTypesRequest) => {
+            try {
+                const response = await axiosClient.get<IBaseModel<IPaginate<ExcerciseDto>>>(GET_EXERCISES_API(subTopicId), { params: query })
+
+                if (!response.data.isSuccess) {
+                    return
+                }
+
+                set((state) => ({
+                    ...state,
+                    excercises: response.data.responseRequest
+                }))
+
+            } catch (error) {
+                console.log('Error fetching excercises', error)
+            }
+        },
+
         bulkExcercise: async (id: string[]) => { },
         createExcercise: async (request: CreateExcerciseRequest) => { },
         updateExcercise: async (request: UpdateExcerciseRequest) => { },
