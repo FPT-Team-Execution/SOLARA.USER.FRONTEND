3e119c4914b508c1bea79386b90c3d26bd0354f1
DatSung <datsung.dev@gmail.com>
2024-12-21 17:10:16 +0700

    feat: display lerning package service

--- solara-user-frontend/src/app/(private)/learning/packages/page.tsx
+++ solara-user-frontend/src/app/(private)/learning/packages/page.tsx
@@ -19,7 +19,7 @@ const Page = () => {
       searchKey: '',
       page: 1,
       size: 100,
-      orderOn: '',
+      orderOn: 'price',
       isAscending: true,
     }
     const response = await axiosClient.get<IBaseModel<IPaginate<LearningPackageDto>>>(GET_LEARNING_PACKAGES_API, { params: query })
@@ -40,17 +40,37 @@ const Page = () => {
             </div>
           )
           :
-          <>
-            {
-              packages?.items.map((item) => {
-                return (
-                  <>
-                    <PackageCard package={item}></PackageCard>
-                  </>
-                )
-              })
-            }
-          </>
+          <div className='flex justify-between w-full flex-col gap-4'>
+
+            <div className="container mx-auto">
+              <div className="flex flex-wrap">
+                <div className="w-full">
+                  <div className="mx-auto max-w-[510px] text-center">
+                    <span className="mb-2 block text-lg font-semibold text-primary">
+                      Bảng giá
+                    </span>
+                    <h2 className="mb-3 text-3xl font-bold leading-[1.208] text-green-700 sm:text-4xl md:text-[40px]">
+                      Các gói dịch vụ
+                    </h2>
+                    <p className="text-base text-body-color dark:text-dark-6">
+                      Đừng bỏ lỡ cơ hội trải nghiệm hệ thống với mức giá vô cùng hợp lý! Chọn ngay gói dịch vụ phù hợp và khởi đầu hành trình của bạn.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='flex gap-4 flex-wrap w-full justify-evenly '>
+              {
+                packages?.items.map((item) => {
+                  return (
+                    <PackageCard key={item.id} package={item}></PackageCard>
+                  )
+                })
+              }
+            </div>
+
+          </div>
       }
 
     </div>
--- solara-user-frontend/src/components/PackagePage/PackageCard.tsx
+++ solara-user-frontend/src/components/PackagePage/PackageCard.tsx
@@ -1,16 +1,59 @@
-import { LearningPackageDto } from '@/types/package'
-import React from 'react'
+import { LearningPackageDto } from '@/types/package';
+import React from 'react';
+import { Card, Button, Typography, notification } from 'antd';
+import { CiCircleCheck } from 'react-icons/ci';
+
+const { Title, Paragraph } = Typography;
 
 interface IProps {
-  package: LearningPackageDto
+  package: LearningPackageDto;
 }
 
 const PackageCard = (props: IProps) => {
-  return (
-    <div>
-        {props.package.name}
-    </div>
-  )
-}
+  const { package: pkg } = props;
 
-export default PackageCard
+  const formatPrice = (price: number | undefined) => {
+    if (!price) return '₫0';
+    return price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+  };
+
+  return (
+    <Card
+      className="max-w-xs shadow-lg hover:shadow-xl transition-shadow duration-300"
+      hoverable
+      cover={<div className="bg-gray-200 rounded-t-lg"></div>}
+    >
+      <Title level={3} className="text-gray-800 mb-4">
+        {pkg.name}
+      </Title>
+
+      <Paragraph className="text-gray-600 text-sm mb-6 min-h-16">{pkg.description}</Paragraph>
+
+
+      <div className="text-center mb-4">
+        <p className="text-3xl font-extrabold">
+          {formatPrice(pkg.finalPrice)}
+        </p>
+        {pkg.discountPrice && (
+          <p className="text-sm text-gray-500 line-through">
+            {formatPrice(pkg.price)}
+          </p>
+        )}
+        <p className="text-gray-500 text-sm">{pkg.durationDay} ngày</p>
+      </div>
+
+      <Button onClick={() => notification.success({ message: "Bạn muốn mua cái này?" })} className='w-full mb-6 !py-6 !font-bold !text-lg !bg-green-600 !text-white'>Đăng ký ngay</Button>
+
+      <hr className='mb-4' />
+
+      <ul className="text-gray-600 text-left text-sm list-none">
+        {pkg.features?.split(',').map((feature, index) => (
+          <li className='flex items-center gap-2' key={index}><CiCircleCheck className='text-green-500' /> {feature.trim()}</li>
+        ))}
+      </ul>
+
+    </Card>
+  );
+};
+
+export default PackageCard;
