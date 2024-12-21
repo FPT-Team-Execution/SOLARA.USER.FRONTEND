f2b577ebdedc6104606d0304b6385e818c21c7fc
DatSung <datsung.dev@gmail.com>
2024-12-21 15:21:53 +0700

    #	modified:   solara-user-frontend/src/app/(private)/learning/packages/page.tsx #	new file:   solara-user-frontend/src/types/package.ts

--- solara-user-frontend/src/app/(private)/learning/packages/page.tsx
+++ solara-user-frontend/src/app/(private)/learning/packages/page.tsx
@@ -1,9 +1,31 @@
+"use client"
+
+import Spinner from '@/components/UI/Spinner'
+import { useRequest } from 'ahooks'
 import React from 'react'
 
 const Page = () => {
+
+  const { loading } = useRequest(async () => {
+
+  })
+
   return (
-    <div>
-        This is packages page
+    <div className='flex gap-4 h-full'>
+      {
+        loading
+          ?
+          (
+            <div className='h-full w-full flex justify-center items-center'>
+              <Spinner />
+            </div>
+          )
+          :
+          <>
+            
+          </>
+      }
+
     </div>
   )
 }
