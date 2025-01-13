b52994c0b5aaf82d71fdeb79258f33f4c2f4cf23
DatSung <datsung.dev@gmail.com>
2025-01-13 11:42:58 +0700

    feat: title topic for subtopic

--- solara-user-frontend/src/components/SubTopicPage/SubTopicRoadMap.tsx
+++ solara-user-frontend/src/components/SubTopicPage/SubTopicRoadMap.tsx
@@ -2,10 +2,14 @@ import useSubTopicStore from '@/zustand/useSubTopicStore'
 import { useRequest } from 'ahooks';
 // import { FaRegFlag } from 'react-icons/fa';
 import RoadMap from './RoadMap';
+import useTopicStore from '@/zustand/useTopicStore';
+import { Button } from 'antd';
+import { ArrowLeftIcon } from '@heroicons/react/24/solid';
 
 const SubTopicRoadMap = () => {
 
     const { subTopic, subTopics, setSubTopic } = useSubTopicStore();
+    const { topic } = useTopicStore();
 
     const { } = useRequest(async () => {
         if (subTopics?.items[0].id) {
@@ -14,10 +18,21 @@ const SubTopicRoadMap = () => {
     })
 
     return (
-        <div className="bg-gray-100 w-full  flex flex-col justify-center items-center py-10">
+        <div className="bg-gray-100 w-full gap-8 flex flex-col justify-center items-center py-10">
+
+            <div className="flex w-5/6 gap-4 rounded-lg  text-black items-center">
+                <Button
+                    className="!w-12 !h-12 flex items-center justify-center !bg-yellow-300 !text-white !rounded-md"
+                >
+                    <ArrowLeftIcon className="w-6 h-6 text-black" />
+                </Button>
+                <div className='flex flex-col items-start'>
+                    <h1 className='text-2xl font-bold'>Chủ đề <span className='text-green-600'>{topic?.topicName}</span></h1>
+                    <p className='text-gray-600'>{topic?.description}</p>
+                </div>
+            </div>
+
             <div className="flex w-full flex-col items-center">
-
-
 
                 {subTopics?.items.map((item, index) => {
 
@@ -31,7 +46,7 @@ const SubTopicRoadMap = () => {
                                         <span className="text-white text-2xl font-bold">★</span>
                                         {/* <div className="absolute top-0 right-0 w-3 h-3 bg-green-700 rounded-full animate-ping"></div> */}
                                     </div>
-                                    <p className={`text-nowrap min-w-16 px-2 ${item.id === subTopic?.id ?  "text-white" :  "text-black"}`}>{item.name}</p>
+                                    <p className={`text-nowrap min-w-16 p-2 ${item.id === subTopic?.id ? "text-white" : "text-black"}`}>{item.name}</p>
                                 </div>
                             </div>
 
