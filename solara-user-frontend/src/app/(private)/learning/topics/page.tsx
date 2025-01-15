02eb3c8873c359f34d73f247ce8f29618ec47332
DatSung <datsung.dev@gmail.com>
2025-01-15 20:41:01 +0700

    chore: better showing topics

--- solara-user-frontend/src/app/(private)/learning/topics/page.tsx
+++ solara-user-frontend/src/app/(private)/learning/topics/page.tsx
@@ -46,62 +46,60 @@ const Page = () => {
 
   return (
     <div>
-      {
-        loading
-          ?
-          (
-            <div className='h-screen w-full flex justify-center items-center'>
-              <Spinner />
+
+      <div>
+
+        <div className='flex'>
+          <div className='w-1/2'>
+            <div>
+              <h1 className='text-sm font-semibold p-4 text-left'>Đang học</h1>
             </div>
-          )
-          :
-          topics === null
-            ?
-            (
-              <span>There is no topic</span>
-            )
-            :
-            (
-              <div>
+            <div>
+              s
+            </div>
+          </div>
+          <div className='w-1/2'>
+            <div>
+              <h1 className='text-sm font-semibold p-4 text-left'>Hoàn thành</h1>
+            </div>
+            <div>
+              s
+            </div>
+          </div>
+        </div>
 
-                <div className='flex'>
-                  <div className='w-1/2'>
-                    <h1 className='text-sm font-semibold p-4 text-left'>Đang học</h1>
-                  </div>
-                  <div className='w-1/2'>
-                    <h1 className='text-sm font-semibold p-4 text-left'>Hoàn thành</h1>
-                  </div>
+        <div className='space-y-4'>
+          <div>
+            <h1 className='text-sm font-semibold p-4 text-left'>Các chủ đề phổ biến</h1>
+          </div>
+
+          <div className='flex justify-center items-center'>
+            <TopicQuery query={query} updateQuery={updateQuery} />
+          </div>
+
+          <div className='flex gap-4 flex-wrap justify-center items-center'>
+            {
+              loading ?
+                <div className='mt-8'>
+                  <Spinner />
                 </div>
+                :
+                topics ?
+                  topics?.items.map((item, index) => {
+                    return (
+                      <div className='cursor-pointer w-full sm:w-1/2 md:w-1/3 max-w-[300px]' key={index} onClick={() => handleNavigate(item.topicId)}>
+                        <TopicCard topic={item} />
+                      </div>
+                    );
+                  })
+                  :
+                  <span>There is no topic</span>
+            }
+          </div>
+        </div>
 
-                <div className='flex'>
-                  <div>
-                    <h1 className='text-sm font-semibold p-4 text-left'>Yêu thích</h1>
-                  </div>
-                </div>
+      </div>
 
-                <div className='space-y-4'>
-                  <div>
-                    <h1 className='text-sm font-semibold p-4 text-left'>Các chủ đề phổ biến</h1>
-                  </div>
-
-                  <div className='flex justify-center items-center'>
-                    <TopicQuery query={query} updateQuery={updateQuery} />
-                  </div>
-
-                  <div className='flex gap-4 flex-wrap justify-center items-center'>
-                    {topics?.items.map((item, index) => {
-                      return (
-                        <div className='cursor-pointer w-full sm:w-1/2 md:w-1/3 max-w-[300px]' key={index} onClick={() => handleNavigate(item.topicId)}>
-                          <TopicCard topic={item} />
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-
-              </div>
-            )
-      }
     </div>
   );
 }
