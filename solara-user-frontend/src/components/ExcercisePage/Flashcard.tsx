4887a241d00ed585461725179d1ebb79024c6156
DatSung <datsung.dev@gmail.com>
2025-01-16 16:25:53 +0700

    feat: showing image for flashcard

--- solara-user-frontend/src/components/ExcercisePage/Flashcard.tsx
+++ solara-user-frontend/src/components/ExcercisePage/Flashcard.tsx
@@ -1,5 +1,5 @@
 import { ExcerciseDto } from '@/types/excercise'
-import React from 'react'
+import Image from 'next/image'
 
 interface IProps {
     flip: boolean
@@ -13,11 +13,19 @@ const Flashcard = ({ handleFlip, excercise, flip }: IProps) => {
             <div className={`relative h-full w-full rounded-xl bg-gray-200 transition-all duration-500 [transform-style:preserve-3d] ${flip ? '[transform:rotateY(180deg)]' : ''}`}>
                 {/* Front Face */}
                 <div className="absolute inset-0 h-full w-full rounded-xl [backface-visibility:hidden]">
-                    <div className="flex min-h-full flex-col items-center justify-center">
-                        <h2 className="text-2xl font-bold mb-4">Câu hỏi:</h2>
-                        <p className="text-lg text-pretty text-center mb-4">
+                    <div className="flex min-h-full flex-col items-center justify-center gap-2">
+
+                        <h2 className="text-2xl font-bold">Câu hỏi:</h2>
+
+                        {
+                            excercise.imageUrl &&
+                            <Image src={excercise.imageUrl} alt='' width={500} height={500} className="w-2/6 h-auto object-cover rounded-lg" loading='eager' />
+                        }
+
+                        <p className="text-lg text-pretty text-center">
                             {excercise?.question}
                         </p>
+
                     </div>
                 </div>
                 {/* Back Face */}
@@ -52,7 +60,6 @@ const Flashcard = ({ handleFlip, excercise, flip }: IProps) => {
                                         }
                                     </div>
                                 )
-
                         }
 
                     </div>
