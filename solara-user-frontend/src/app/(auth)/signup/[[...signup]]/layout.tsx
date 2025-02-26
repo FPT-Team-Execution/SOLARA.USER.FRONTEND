53d8c0dee827f33ab363eca8e6990ee286c2892d
DatSung <datsung.dev@gmail.com>
2025-02-26 08:35:36 +0700

    feat: update content description seo

--- solara-user-frontend/src/app/(auth)/signup/[[...signup]]/layout.tsx
+++ solara-user-frontend/src/app/(auth)/signup/[[...signup]]/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: "Sign Up - Solara",
-    description: "Learning survival course application",
+    description: "Solara - Học kỹ năng sinh tồn MIỄN PHÍ qua flashcard trực quan. Trang bị kiến thức thoát hiểm, sơ cứu và sinh tồn trong mọi tình huống khẩn cấp. Truy cập ngay để bảo vệ bản thân và gia đình!",
     icons: {
         icon: '../../',
     },
