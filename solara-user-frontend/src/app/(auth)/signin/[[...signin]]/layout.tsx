d8fd2069ca121e31249fe9468b74dcbb485b8eae
DatSung <datsung.dev@gmail.com>
2025-02-26 08:41:26 +0700

    feat: fix content for seo adding keywords

--- solara-user-frontend/src/app/(auth)/signin/[[...signin]]/layout.tsx
+++ solara-user-frontend/src/app/(auth)/signin/[[...signin]]/layout.tsx
@@ -6,6 +6,20 @@ export const metadata: Metadata = {
     icons: {
         icon: '../../',
     },
+    keywords: 'kỹ năng sinh tồn, năng lượng mặt trời, tạo lửa, tìm nước, sống sót, kỹ năng sống',
+    authors: { name: "Solara" },
+    openGraph: {
+        title: 'Solara - Kỹ Năng Sinh Tồn',
+        description: 'Học các kỹ năng sinh tồn thiết yếu để xử lí khi gặp tình huống khẩn cấp.',
+        images: [
+            {
+                url: 'https://solara.io.vn/_next/image?url=%2Flogo.png&w=256&q=75',
+                width: 256,
+                height: 256,
+            },
+        ],
+        url: 'https://solara.io.vn/',
+    },
 };
 
 const AuthLayout = ({ children }: Readonly<{ children: React.ReactNode; }>) => {
