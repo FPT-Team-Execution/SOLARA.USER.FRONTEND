c0e797c318914af1baa4f7137d7d31c091e8037b
Nguyen Thanh Dat <datsung.dev@gmail.com>
2025-03-28 01:24:29 +0700

    feat: check userSubcription duration before using  feature

--- solara-user-frontend/src/app/(private)/learning/threats/page.tsx
+++ solara-user-frontend/src/app/(private)/learning/threats/page.tsx
@@ -6,6 +6,7 @@ import { LEARNING_PACKAGES_ROUTE } from "@/constants/routes"
 import { LocationPrediction } from "@/types/prediction"
 import useUserStore from "@/zustand/useUserStore"
 import { useRequest } from "ahooks"
+import { notification } from "antd"
 import { useRouter } from 'next/navigation';
 import { useState } from "react"
 
@@ -15,19 +16,35 @@ const Page = () => {
   const router = useRouter();
   const { userSubcriptions, getUserSubsctiptions } = useUserStore();
   const { } = useRequest(async () => {
+    await getUserSubsctiptions();
+
     if (userSubcriptions == null || userSubcriptions.length == 0) {
-      await getUserSubsctiptions();
+      notification.warning({
+        message: 'Chức năng chưa mở khóa!',
+        description: "Bạn cần mua gói để tiếp tục!"
+      })
+      router.push(LEARNING_PACKAGES_ROUTE)
+      return
     }
+
     const duration = Math.max(
       0,
       Math.ceil(
-          (new Date(userSubcriptions![0].endDate).getTime() -
-              new Date().getTime()) /
-          (1000 * 60 * 60 * 24)
+        (new Date(userSubcriptions![0].endDate).getTime() -
+          new Date().getTime()) /
+        (1000 * 60 * 60 * 24)
       )
-  )
+    )
+
+    console.log(duration)
+
     if (duration <= 0) {
+      notification.warning({
+        message: 'Gói đã hết hạn!',
+        description: "Bạn cần gia hạn gói để tiếp tục!"
+      })
       router.push(LEARNING_PACKAGES_ROUTE)
+      return
     }
   });
 
